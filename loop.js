0aa8eac8e2a8325949f8ebb454eed0b96c5cb55e
masudtak <masud08eee@gmail.com>
2023-07-31 22:30:07 -0400

    solved loop problems

---
+++ loop.js
@@ -0,0 +1,103 @@
+/* // Display “Ajke amar mon valo nai” for 39 times
+// using for loop
+
+/* for(var i =1 ; i<=39 ; i++){
+    console.log(i + ": Ajke amar mon valo nai ");
+} */
+
+//using while loop
+/* console.log('while loop')
+
+var i=1;
+while(i<=39){
+    console.log(i + ": Ajke amar mon valo nai ");
+    i++
+} */
+
+// Display numbers between 58 to 98
+/* 
+for(var i=58; i<=98; i++){
+    console.log(i);
+} */
+
+// using while loop
+/* var i = 58;
+while(i<= 98){
+    console.log(i);
+    i++;
+} */
+
+//Show all even numbers from 412 to 456
+
+/* for(var i= 412; i<= 456; i+= 2){
+    console.log(i)
+} */
+
+/* var i = 412;
+while(i <= 456){
+    console.log(i);
+    i+=2;
+}
+ */
+// Show all odd numbers from 581 to 623
+
+/* for(var i = 581; i <= 623 ; i+= 2){
+    console.log(i);
+} */
+
+/* var i = 581;
+while(i<=623){
+    console.log(i);
+    i+= 2
+} */
+
+//Difference between while and for loop
+
+//Declare an array for all the topics that you have learned last few days .Display them as output
+
+/* var webDevelopment = ["html5", "CSS3", "TalwindCSS", "DaisyUI", "JavaScript", "git"];
+for(var i= 0; i< webDevelopment.length; i++){
+    console.log(webDevelopment[i]);
+} */
+
+ 
+/* Create an array for all the mobile phone you have used, 
+Display all element of the array by using while loop. */
+ 
+/* var phoneUsed = ["Nokia", "Samsung", "htc", "iphone"];
+ var i = 0;
+ while(i< phoneUsed.length){
+    console.log(phoneUsed[i])
+    i++;
+ }
+ */
+
+ //using whileloop
+/*  var phoneUsed = ["Nokia", "Samsung", "htc", "iphone"];
+ var i = 0;
+ while(i< phoneUsed.length){
+    console.log(phoneUsed[i]);
+    i++;
+ } */
+
+
+ //Run a loop from 30 to 86. This loop will stop if the value gater than 48.
+//  var numbers = [30,  35, 38,40, 45 , 48, 55, 56, 60 ]
+
+/* for( var i =30; i<=86; i++){
+    if(i >= 48){
+        break
+    }
+    console.log(i)
+} */
+
+//Write the price of the books that you have , Display the price if the price is lower than 200
+
+ 
+/*  var bookPrice = [100,1000 ,150, 198, 185, 200, 250 , 500, 600,199, 205];
+for( var i = 0 ; i<bookPrice.length; i++){
+    if(bookPrice[i] >= 200){
+        continue;
+    }
+    console.log(bookPrice[i])
+} */
