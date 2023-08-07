bc98a409cf1fe01c8ad34c0099593e2ef17ca6ce
masudtak <masud08eee@gmail.com>
2023-08-07 03:05:54 -0400

    add some more

--- validation.js
+++ validation.js
@@ -1,15 +1,15 @@
-function sum(num1, num2){
-    if(typeof num1 !== "number" || typeof num2 !== "number"){
-        return "Please enter a number "
-    }
-    return num1 + num2;
+  function sum(num1, num2) {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "Please enter a number ";
+  }
+  return num1 + num2;
 }
-const result = sum(15,"45");
+const result = sum(15, "45");
 console.log(result);
 
-function multiply(num1, num2){
-    return num1 * num2;
-
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 const multiplication = multiply(10, 15);
-console.log(multiplication);
+console.log(multiplication); 
+ 
